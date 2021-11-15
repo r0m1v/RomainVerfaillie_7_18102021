c8f6aa41798ec999a7f988ca55f33b8ff57963e6
Rom1v <rom1.verfaillie@gmail.com>
2021-11-15 17:28:10 +0100

    Ajout package yup

--- frontend/react-projet7/src/pages/Inscription.js
+++ frontend/react-projet7/src/pages/Inscription.js
@@ -1,25 +1,64 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import Navigation from "../components/Navigation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  userName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
 
 const Inscription = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
+  const { register, handleSubmit, formState, errors } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+  const { isSubmitting, isSubmitSuccessful } = formState;
+  const onSubmit = (data) => {
     console.log(data);
-  }
+  };
+
+  console.log(errors);
+  
   return (
     <div>
       <Navigation />
       <div className="formatting-form">
         <form className="inscr-form" onSubmit={handleSubmit(onSubmit)}>
+          {isSubmitSuccessful && <span>Merci pour l'inscription</span>}
+          <br />
           <label htmlFor="userName">PSEUDO * </label>
-          <input type="text" name="userName" id="username" {...register("userName") } required />
+          <input
+            defaultValue={"JohnDoe"}
+            type="text"
+            name="userName"
+            id="username"
+            {...register("userName")}
+          />
           <label htmlFor="email">ADRESSE EMAIL * </label>
-          <input type="text" name="email" id="email" {...register("email")} required />
+          <input
+            defaultValue={"johndoe@gmail.com"}
+            type="text"
+            name="email"
+            id="email"
+            {...register("email")}
+          />
           <label htmlFor="password">MOT DE PASSE * </label>
-          <input type="password" name="password" id="password" {...register("password")} required />
+          <input
+            defaultValue={"1234"}
+            type="password"
+            name="password"
+            id="password"
+            {...register("password")}
+          />
           <div className="formatting-form">
-            <button type="submit" className="button-form formatting-form">
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className="button-form formatting-form"
+            >
               S'INSCRIRE
             </button>
           </div>
